ff36aefae5d48639503937453926f396617a0a68
Marcos Gui <marcos.gui@spsoft-cn.com>
2019-01-30 17:37:54 +0800

    [IMP] Init spring-security-acl project success

--- src/main/java/com/spring/security/securitydemo/config/AclMethodSecurityConfiguration.java
+++ src/main/java/com/spring/security/securitydemo/config/AclMethodSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.spring.security.securitydemo.config;
 
+import javax.annotation.Resource;
+import org.hibernate.annotations.Source;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * AclMethodSecurityConfiguration:
@@ -15,11 +19,17 @@ import org.springframework.security.config.annotation.method.configuration.Globa
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class AclMethodSecurityConfiguration extends GlobalMethodSecurityConfiguration{
 
-  @Autowired
-  MethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler;
+  @Resource
+   MethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler;
+
 
   @Override
   protected MethodSecurityExpressionHandler createExpressionHandler() {
     return defaultMethodSecurityExpressionHandler;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    super.configure(auth);
   }
 }
--- src/main/java/com/spring/security/securitydemo/domain/NoticeMessage.java
+++ src/main/java/com/spring/security/securitydemo/domain/NoticeMessage.java
@@ -5,23 +5,32 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * NoticeMessage: Entity of NoticeMessage
+ * @author marcosgui
+ */
 @Entity
 @Table(name = "system_message")
 public class NoticeMessage{
+
   @Id
   @Column
   private Integer id;
   @Column
   private String content;
+
   public Integer getId() {
     return id;
   }
+
   public void setId(Integer id) {
     this.id = id;
   }
+
   public String getContent() {
     return content;
   }
+
   public void setContent(String content) {
     this.content = content;
   }
